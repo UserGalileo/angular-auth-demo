c122cade6c5b5593f6df819af9a328f30ba2a645
Michele Stieven <usergalileo@gmail.com>
2022-11-02 00:44:47 +0100

    fix

--- src/app/oauth/oauth-config.ts
+++ src/app/oauth/oauth-config.ts
@@ -3,7 +3,7 @@ import { WebStorageStateStore, UserManagerSettings } from 'oidc-client';
 export const oauthConfig: UserManagerSettings = {
   authority: 'https://accounts.google.com',
   // TODO: Add your Client ID here (https://console.cloud.google.com/apis/credentials)
-  client_id: '229736874798-0vi7knh0kti2sok3g0uvg5l9tdpsdndc.apps.googleusercontent.com',
+  client_id: '',
   redirect_uri: 'http://localhost:4200/auth-callback',
   popup_redirect_uri: 'http://localhost:4200/auth-callback',
   response_type: 'id_token token',
